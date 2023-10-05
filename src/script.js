42e5da8615671a0fb6c1577ac84ebc09249d76cc
movatica <c0d3@movatica.com>
2023-10-06 00:33:41 +0200

    Use const in JS script

--- src/script.js
+++ src/script.js
@@ -1,26 +1,28 @@
 // show a wiki search button for selected text
+'use strict';
+
 jQuery(function(){
-    var tooltip = jQuery('#selectionsearch__tt');
+    const tooltip = jQuery('#selectionsearch__tt');
     if (!tooltip[0]) return;
 
-    var container = jQuery('#dokuwiki__content');
+    const container = jQuery('#dokuwiki__content');
 
     // mouseup callback for content div
     container.on('mouseup', function(){
         tooltip.hide();
 
-        var selection = window.getSelection();
+        const selection = window.getSelection();
 
-        var query = selection.toString().trim();
+        const query = selection.toString().trim();
         if (query.length < JSINFO.selectionsearch_minlength) return;
 
         tooltip.attr('href', '?' + jQuery.param({ id: JSINFO.id, do: 'search', q: query }));
 
         // for some reason, the selection rectangle is weirdly offset and we need to correct this
-        var content_rect = container[0].getBoundingClientRect();
+        const content_rect = container[0].getBoundingClientRect();
         // for a multiline selection, place the tooltip after the last line
-        var selection_rects = selection.getRangeAt(0).getClientRects();
-        var selection_rect = selection_rects[selection_rects.length-1];
+        const selection_rects = selection.getRangeAt(0).getClientRects();
+        const selection_rect = selection_rects[selection_rects.length-1];
 
         tooltip.css({top: selection_rect.bottom-content_rect.top, left: selection_rect.right-content_rect.left});
         tooltip.show();
